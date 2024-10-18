d311f963072bc23eda1d991b0e8b97f2e9931020
HerryOnJourney <2371786722@qq.com>
2024-10-18 11:07:35 +0800

    10月18日

--- vue-antd-admin-master/src/pages/login/Login.vue
+++ vue-antd-admin-master/src/pages/login/Login.vue
@@ -13,60 +13,103 @@
             <a-alert type="error" :closable="true" v-show="error" :message="error" showIcon style="margin-bottom: 24px;" />
             <a-form-item>
               <a-input
-                autocomplete="autocomplete"
-                size="large"
-                placeholder="admin"
-                v-decorator="['name', {rules: [{ required: true, message: '请输入账户名', whitespace: true}]}]"
+                  autocomplete="autocomplete"
+                  size="large"
+                  placeholder="账户"
+                  v-decorator="['name', {rules: [{ required: true, message: '请输入账户名', whitespace: true}]}]"
               >
                 <a-icon slot="prefix" type="user" />
               </a-input>
             </a-form-item>
             <a-form-item>
               <a-input
-                size="large"
-                placeholder="888888"
-                autocomplete="autocomplete"
-                type="password"
-                v-decorator="['password', {rules: [{ required: true, message: '请输入密码', whitespace: true}]}]"
+                  size="large"
+                  placeholder="密码"
+                  autocomplete="autocomplete"
+                  type="password"
+                  v-decorator="['password', {rules: [{ required: true, message: '请输入密码', whitespace: true}]}]"
               >
                 <a-icon slot="prefix" type="lock" />
               </a-input>
             </a-form-item>
-          </a-tab-pane>
-          <!-- <a-tab-pane tab="手机号登录" key="2">
+            <a-radio-group v-model="userRole">
+              <a-radio value="admin">管理员</a-radio>
+              <a-radio value="student">学生</a-radio>
+            </a-radio-group>
+            <!--              <a-select-->
+            <!--                  size="large"-->
+            <!--                  placeholder="请选择角色"-->
+            <!--                  v-decorator="['userRole', {rules: [{ required: true, message: '请选择角色'}]}]"-->
+            <!--                  allowClear-->
+            <!--              >-->
+            <!--                <a-select-option value="admin">管理员</a-select-option>-->
+            <!--                <a-select-option value="student">学生</a-select-option>-->
+            <!--              </a-select>-->
             <a-form-item>
-              <a-input size="large" placeholder="mobile number" >
-                <a-icon slot="prefix" type="mobile" />
+              <a-button :loading="logging" style="width: 100%;margin-top: 24px" size="large" htmlType="submit" type="primary">登录</a-button>
+            </a-form-item>
+          </a-tab-pane>
+          <a-tab-pane tab="注册" key="2">
+            <a-alert type="error" :closable="true" v-show="error" :message="error" showIcon style="margin-bottom: 24px;" />
+            <a-form-item>
+              <a-input
+                  autocomplete="autocomplete"
+                  size="large"
+                  placeholder="账户"
+                  v-decorator="['name', {rules: [{ required: true, message: '请输入账户名', whitespace: true}]}]"
+              >
+                <a-icon slot="prefix" type="user" />
               </a-input>
             </a-form-item>
             <a-form-item>
-              <a-row :gutter="8" style="margin: 0 -4px">
-                <a-col :span="16">
-                  <a-input size="large" placeholder="captcha">
-                    <a-icon slot="prefix" type="mail" />
-                  </a-input>
-                </a-col>
-                <a-col :span="8" style="padding-left: 4px">
-                  <a-button style="width: 100%" class="captcha-button" size="large">获取验证码</a-button>
-                </a-col>
-              </a-row>
+              <a-input
+                  size="large"
+                  placeholder="密码"
+                  autocomplete="autocomplete"
+                  type="password"
+                  v-decorator="['password', {rules: [{ required: true, message: '请输入密码', whitespace: true}]}]"
+              >
+                <a-icon slot="prefix" type="lock" />
+              </a-input>
             </a-form-item>
-          </a-tab-pane> -->
+            <a-form-item>
+              <a-button :loading="logging" style="width: 100%;margin-top: 24px" size="large" htmlType="submit" type="primary">注册</a-button>
+            </a-form-item>
+          </a-tab-pane>
+          <!--          <a-tab-pane tab="手机号登录" key="2">-->
+          <!--            <a-form-item>-->
+          <!--              <a-input size="large" placeholder="mobile number" >-->
+          <!--                <a-icon slot="prefix" type="mobile" />-->
+          <!--              </a-input>-->
+          <!--            </a-form-item>-->
+          <!--            <a-form-item>-->
+          <!--              <a-row :gutter="8" style="margin: 0 -4px">-->
+          <!--                <a-col :span="16">-->
+          <!--                  <a-input size="large" placeholder="captcha">-->
+          <!--                    <a-icon slot="prefix" type="mail" />-->
+          <!--                  </a-input>-->
+          <!--                </a-col>-->
+          <!--                <a-col :span="8" style="padding-left: 4px">-->
+          <!--                  <a-button style="width: 100%" class="captcha-button" size="large">获取验证码</a-button>-->
+          <!--                </a-col>-->
+          <!--              </a-row>-->
+          <!--            </a-form-item>-->
+          <!--          </a-tab-pane>-->
         </a-tabs>
-        <div>
-          <!-- <a-checkbox :checked="true" >自动登录</a-checkbox>
-          <a style="float: right">忘记密码</a> -->
-        </div>
-        <a-form-item>
-          <a-button :loading="logging" style="width: 100%;margin-top: 24px" size="large" htmlType="submit" type="primary">登录</a-button>
-        </a-form-item>
-        <!-- <div>
-          其他登录方式
-          <a-icon class="icon" type="alipay-circle" />
-          <a-icon class="icon" type="taobao-circle" />
-          <a-icon class="icon" type="weibo-circle" />
-          <router-link style="float: right" to="/dashboard/workplace" >注册账户</router-link>
-        </div> -->
+        <!--        <div>-->
+        <!--          <a-checkbox :checked="true" >自动登录</a-checkbox>-->
+        <!--          <a style="float: right">忘记密码</a>-->
+        <!--        </div>-->
+        <!--        <a-form-item>
+                  <a-button :loading="logging" style="width: 100%;margin-top: 24px" size="large" htmlType="submit" type="primary">登录</a-button>
+                </a-form-item>-->
+        <!--        <div>-->
+        <!--          其他登录方式-->
+        <!--          <a-icon class="icon" type="alipay-circle" />-->
+        <!--          <a-icon class="icon" type="taobao-circle" />-->
+        <!--          <a-icon class="icon" type="weibo-circle" />-->
+        <!--          <router-link style="float: right" to="/dashboard/workplace" >注册账户</router-link>-->
+        <!--        </div>-->
       </a-form>
     </div>
   </common-layout>
@@ -86,7 +129,9 @@ export default {
     return {
       logging: false,
       error: '',
-      form: this.$form.createForm(this)
+      form: this.$form.createForm(this),
+      // 默认角色
+      userRole: 'admin'
     }
   },
   computed: {
@@ -102,8 +147,11 @@ export default {
         if (!err) {
           this.logging = true
           const name = this.form.getFieldValue('name')
-          const password = this.form.getFieldValue('password')
-          login(name, password).then(this.afterLogin)
+          const password = this.form.getFieldValue('password');
+
+          // 获取所选角色
+          const role = this.userRole;
+          login(name, password, role).then(this.afterLogin);
         }
       })
     },
@@ -132,59 +180,59 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .common-layout{
-    .top {
-      text-align: center;
-      .header {
-        height: 44px;
-        line-height: 44px;
-        a {
-          text-decoration: none;
-        }
-        .logo {
-          height: 44px;
-          vertical-align: top;
-          margin-right: 16px;
-        }
-        .title {
-          font-size: 33px;
-          color: @title-color;
-          font-family: 'Myriad Pro', 'Helvetica Neue', Arial, Helvetica, sans-serif;
-          font-weight: 600;
-          position: relative;
-          top: 2px;
-        }
+.common-layout{
+  .top {
+    text-align: center;
+    .header {
+      height: 44px;
+      line-height: 44px;
+      a {
+        text-decoration: none;
       }
-      .desc {
-        font-size: 14px;
-        color: @text-color-second;
-        margin-top: 12px;
-        margin-bottom: 40px;
+      .logo {
+        height: 44px;
+        vertical-align: top;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 33px;
+        color: @title-color;
+        font-family: 'Myriad Pro', 'Helvetica Neue', Arial, Helvetica, sans-serif;
+        font-weight: 600;
+        position: relative;
+        top: 2px;
       }
     }
-    .login{
-      width: 368px;
-      margin: 0 auto;
-      @media screen and (max-width: 576px) {
-        width: 95%;
+    .desc {
+      font-size: 14px;
+      color: @text-color-second;
+      margin-top: 12px;
+      margin-bottom: 40px;
+    }
+  }
+  .login{
+    width: 368px;
+    margin: 0 auto;
+    @media screen and (max-width: 576px) {
+      width: 95%;
+    }
+    @media screen and (max-width: 320px) {
+      .captcha-button{
+        font-size: 14px;
       }
-      @media screen and (max-width: 320px) {
-        .captcha-button{
-          font-size: 14px;
-        }
-      }
-      .icon {
-        font-size: 24px;
-        color: @text-color-second;
-        margin-left: 16px;
-        vertical-align: middle;
-        cursor: pointer;
-        transition: color 0.3s;
+    }
+    .icon {
+      font-size: 24px;
+      color: @text-color-second;
+      margin-left: 16px;
+      vertical-align: middle;
+      cursor: pointer;
+      transition: color 0.3s;
 
-        &:hover {
-          color: @primary-color;
-        }
+      &:hover {
+        color: @primary-color;
       }
     }
   }
+}
 </style>
