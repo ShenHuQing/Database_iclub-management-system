b94eec75f72713df7ef3571438b0d86a5a6b47a9
HerryOnJourney <2371786722@qq.com>
2024-11-15 14:52:26 +0800

    commonLayOut

--- vue-antd-admin-master/src/layouts/CommonLayout.vue
+++ vue-antd-admin-master/src/layouts/CommonLayout.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="common-layout">
     <div class="content"><slot></slot></div>
-    <page-footer :link-list="footerLinks" :copyright="copyright"></page-footer>
+<!--    <page-footer :link-list="footerLinks" :copyright="copyright"></page-footer>-->
   </div>
 </template>
 
 <script>
-import PageFooter from '../layouts/footer/PageFooter'
-import {mapState} from 'vuex'
+// import PageFooter from '../layouts/footer/PageFooter'
+// import {mapState} from 'vuex'
 
 export default {
   name: 'CommonLayout',
-  components: {PageFooter},
-  computed: {
-    ...mapState('setting', ['footerLinks', 'copyright'])
-  }
+  // components: {PageFooter},
+  // computed: {
+  //   ...mapState('setting', ['footerLinks', 'copyright'])
+  // }
 }
 </script>
 
